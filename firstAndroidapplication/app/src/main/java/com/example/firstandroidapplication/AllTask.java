4c243ec765c24761b03a0e8f1eb8b9add34a1b3e
AhmadSailik <asailk1993@gmail.com>
2021-08-31 16:43:06 +0300

    solve lab28

--- firstAndroidapplication/app/src/main/java/com/example/firstandroidapplication/AllTask.java
+++ firstAndroidapplication/app/src/main/java/com/example/firstandroidapplication/AllTask.java
@@ -1,8 +1,15 @@
 package com.example.firstandroidapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import java.util.ArrayList;
 
 public class AllTask extends AppCompatActivity {
 
@@ -10,5 +17,26 @@ public class AllTask extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_task);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        ArrayList<Task>allTask=new ArrayList<Task>();
+        allTask.add(new Task("jop","still research","in progress"));
+        allTask.add(new Task("lab","add All task","complete"));
+        allTask.add(new Task("CSS","course CSS","assigned"));
+        RecyclerView allTaskRecyclerView=findViewById(R.id.listOfTask);
+        allTaskRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+//        allTaskRecyclerView.setAdapter(new vickAdapter(allTask));
+        allTaskRecyclerView.setAdapter(new TaskAdapter(allTask));
+
+    }
+    public boolean onOptionsItemSelected(MenuItem item){
+        Intent myIntent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivityForResult(myIntent, 0);
+        return true;
     }
 }
---
+++ firstAndroidapplication/app/src/main/java/com/example/firstandroidapplication/TaskAdapter.java
@@ -0,0 +1,52 @@
+package com.example.firstandroidapplication;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.TaskViewHolder> {
+    List<Task>allTask=new ArrayList<Task>();
+    public TaskAdapter(List<Task>allTask){
+        this.allTask=allTask;
+    }
+
+    public static class TaskViewHolder extends RecyclerView.ViewHolder{
+        public Task task;
+        View itemView;
+
+        public TaskViewHolder(@NonNull  View itemView) {
+            super(itemView);
+            this.itemView=itemView;
+        }
+    }
+    @NonNull
+    @Override
+    public TaskViewHolder onCreateViewHolder(@NonNull  ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_task,parent,false);
+        TaskViewHolder taskViewHolder=new TaskViewHolder(view);
+        return taskViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TaskAdapter.TaskViewHolder holder, int position) {
+        holder.task=allTask.get(position);
+        TextView titleOfTask=holder.itemView.findViewById(R.id.titleOfTask);
+        TextView bodyOfTask=holder.itemView.findViewById(R.id.bodyOfTask);
+        TextView stateOfTask=holder.itemView.findViewById(R.id.stateOfTask);
+        titleOfTask.setText(holder.task.title);
+        bodyOfTask.setText(holder.task.body);
+        stateOfTask.setText(holder.task.state);
+    }
+
+    @Override
+    public int getItemCount() {
+        return allTask.size();
+    }
+}
